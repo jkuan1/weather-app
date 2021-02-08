91aa1a2c33ef175b48412118cf9bbd6777dccc86
jkuan1 <justinkuan2000@gmail.com>
2021-02-07 16:00:20 -0800

    added template weather component to weather-app and adjusted app routing to the component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WeatherComponent } from './weather/weather.component';
 
+export const allAppRoutes: Routes = [
+  { path: '', component: WeatherComponent }
+];
 const routes: Routes = [];
 
 @NgModule({
